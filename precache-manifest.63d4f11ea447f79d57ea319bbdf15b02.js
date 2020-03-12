67d41dab3bde73b5eaf302c9d746e1f3c3b6528b
Oscar Björkgren <osbjorkg@abo.fi>
2020-03-13 00:33:11 +0200

    Updates

---
+++ precache-manifest.63d4f11ea447f79d57ea319bbdf15b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec6e7de64e93a81236c1bfec7af7317",
+    "url": "/com-finlandchinanews-cli/index.html"
+  },
+  {
+    "revision": "227a22b355e5e4a60ff6",
+    "url": "/com-finlandchinanews-cli/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "227a22b355e5e4a60ff6",
+    "url": "/com-finlandchinanews-cli/static/js/2.dc313cb1.chunk.js"
+  },
+  {
+    "revision": "db16463b5666b4a6f22ca1dcc8b9eaf4",
+    "url": "/com-finlandchinanews-cli/static/js/2.dc313cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fc512a88d19882c9b8",
+    "url": "/com-finlandchinanews-cli/static/js/main.9a0044a2.chunk.js"
+  },
+  {
+    "revision": "da779b82a6f14858e26a",
+    "url": "/com-finlandchinanews-cli/static/js/runtime-main.146d8daf.js"
+  }
+]);
